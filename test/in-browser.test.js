180aaf951dfe3add89b95597a755122b7ff147a3
Karlen Manaseryan <kmanaseryan@gmail.com>
2020-09-09 21:19:39 +0400

    [tests][s]: Write tests for testing  addSchema() and stream() methods of file having browser File type - #49

--- test/in-browser.test.js
+++ test/in-browser.test.js
@@ -12,6 +12,7 @@ const nock = require('nock')
 const toArray = require('stream-to-array')
 
 const data = require('../src/index')
+const { testFileStream } = require('./index.test')
 
 test('FileInterface size and hash', async (t) => {
   const file = new data.open(
@@ -21,5 +22,47 @@ test('FileInterface size and hash', async (t) => {
   const hash = await file.hash()
 
   t.is(hash, '552e21cd4cd9918678e3c1a0df491bc3')
+  t.is(fileLocal.displayName, 'FileInterface')
   t.is(file.size, 9)
+})
+
+test('FileInterface stream()', async (t) => {
+  const file = new data.open(
+    new File(
+      [
+        `
+    number,string,boolean
+    1,two,true
+    3,four,false
+    `,
+      ],
+      'sample.csv',
+      { type: 'text/csv' }
+    )
+  )
+
+  await testFileStream(t, res)
+})
+
+test('FileInterface addSchema()', async (t) => {
+  const file = new data.open(
+    new File(
+      [
+        `
+      number,string,boolean
+      1,two,true
+      3,four,false
+      `,
+      ],
+      'sample.csv',
+      { type: 'text/csv' }
+    )
+  )
+  t.is(file.descriptor.schema, undefined)
+  await file.addSchema()
+  t.is(file.descriptor.schema.fields[1].type, 'string')
+  let headers = file.descriptor.schema.fields.map((field) => field.name)
+  t.deepEqual(headers, ['number', 'string', 'boolean'])
+
+  await testFileStream(t, res)
 })
