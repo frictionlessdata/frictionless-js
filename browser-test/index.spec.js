298fcf86a8a4c5c7ad94c5aa8a0d66e0867f623c
Karlen Manaseryan <kmanaseryan@gmail.com>
2020-09-11 10:28:50 +0400

    [test][m]: Update the test to cover hash sha256 and rows() functions - #49

--- browser-test/index.spec.js
+++ browser-test/index.spec.js
@@ -1,18 +1,21 @@
 const toArray = require('stream-to-array')
 
+const genFile = () => {
+  return new File(
+    [
+      `number,string,boolean
+1,two,true
+3,four,false
+`,
+    ],
+    'sample.csv',
+    { type: 'text/csv' }
+  )
+}
+
 describe('FileInterface', function () {
   it('addSchema()', async () => {
-    const file = new data.open(
-      new File(
-        [
-          `number,string,boolean
-1,two,true
-3,four,false`,
-        ],
-        'sample.csv',
-        { type: 'text/csv' }
-      )
-    )
+    const file = new data.open(genFile())
 
     expect(file.descriptor.schema).toBe(undefined)
     await file.addSchema()
@@ -22,31 +25,21 @@ describe('FileInterface', function () {
   })
 
   it('displayName, size and hash', async () => {
-    const file = new data.open(
-      new File(['some text'], 'test.txt', { type: 'text/plain' })
-    )
+    const file = new data.open(genFile())
 
     const hash = await file.hash()
-    expect(hash).toBe('552e21cd4cd9918678e3c1a0df491bc3')
+    const hashSha256 = await file.hashSha256()
+    expect(hash).toBe('sGYdlWZJioAPv5U2XOKHRw==')
+    expect(hashSha256).toBe('2dR7kKyWB8URH/moOqN7wQ4FjOYgbAC2YmreCReE4Jg=')
     expect(file.displayName).toBe('FileInterface')
-    expect(file.size).toBe(9)
+    expect(file.size).toBe(46)
   })
 
   it('rows()', async () => {
-    const file = new data.open(
-      new File(
-        [
-          `
-number,string,boolean
-1,two,true
-3,four,false`,
-        ],
-        'sample.csv',
-        { type: 'text/csv' }
-      )
-    )
+    debugger
+    const file = new data.open(genFile())
 
-    const rowStream = await file.rows()
+    const rowStream = await file.rows({ size: -1 })
     const rows = await toArray(rowStream)
     expect(rows[0]).toEqual(['number', 'string', 'boolean'])
     expect(rows[1]).toEqual(['1', 'two', 'true'])
