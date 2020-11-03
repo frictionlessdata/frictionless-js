f55f5bbbf8a003cede0a979c4a0aab5a42e47a9a
Rising Odegua <risingodegua@gmail.com>
2020-11-03 15:56:01 +0100

    [.gitignore][s]: Fix bug in CI

---
+++ src/index.js
@@ -0,0 +1,15 @@
+export {
+  open,
+  parsePath,
+  parseDatasetIdentifier,
+  isDataset,
+  isUrl,
+} from './data'
+export { File } from './file-base'
+export { FileInterface } from './file-interface'
+export { FileLocal } from './file-local'
+export { FileRemote } from './file-remote'
+export { FileInline } from './file-inline'
+export { Dataset } from './dataset'
+export { csvParser } from './parser/csv'
+export { xlsxParser } from './parser/xlsx'
